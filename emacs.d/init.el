fcb2fb25cb55811672e57e545bd951d34f6c174a
Petteri Kääpä <petteri.kaapa@gmail.com>
2012-07-31 17:43:18 +0300

    Clean whitespace

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,16 +3,16 @@
 (require 'dotfiles-packages)
 
 (dotfiles-install-packages '(color-theme-solarized
-			     css-mode
-			     haml-mode
-			     js2-mode
-			     magit
-			     markdown-mode
-			     rinari
-			     sass-mode
-			     scss-mode
-			     yaml-mode))
+                             css-mode
                              expand-region
+                             haml-mode
+                             js2-mode
+                             magit
+                             markdown-mode
+                             rinari
+                             sass-mode
+                             scss-mode
+                             yaml-mode))
 
 (load-theme 'solarized-dark t)
 
